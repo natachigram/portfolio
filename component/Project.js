0aeb04ede11cad67ece52804cc7bb993922e7ba9
natachigram <natachigram@gmail.com>
2022-05-25 18:47:39 +0100

    Added a new project

--- component/Project.js
+++ component/Project.js
@@ -5,6 +5,7 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import projectStyle from '../styles/Project.module.scss';
 import netflix from '../public/netflix.png';
 import tinder from '../public/tinder.png';
+import blog from '../public/blog.png';
 import Fade from 'react-reveal/Fade';
 
 function Project() {
@@ -22,8 +23,44 @@ function Project() {
           <div className={projectStyle.projects__container}>
             <div className={projectStyle.project__card}>
               <div className={projectStyle.project__thumbnail}>
+                <Link href='https://natachi-blog.vercel.app/'>
+                  <a className={projectStyle.thumb} target='_blank'>
+                    <Image
+                      src={blog}
+                      layout='fill'
+                      className={projectStyle.image}
+                    />
+                    <div></div>
+                  </a>
+                </Link>
+              </div>
+              <div className={projectStyle.project__description}>
+                <h4>Blog Web-App</h4>
+                <div className={projectStyle.textSummary}>
+                  <span>A Blog Web-App built with NextJS and SanityCMS</span>
+                </div>
+                <div className={projectStyle.technologies}>
+                  <span>NextJS Typescript Sanity-CMS </span>
+                </div>
+                <div className={projectStyle.links}>
+                  <Link href='https://natachi-blog.vercel.app/'>
+                    <a target='_blank'>
+                      <LaunchIcon />
+                    </a>
+                  </Link>
+                  <Link href='https://github.com/natachigram/natachi-blog'>
+                    <a target='_blank'>
+                      <GitHubIcon />
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className={projectStyle.project__card}>
+              <div className={projectStyle.project__thumbnail}>
                 <Link href='https://netflix-clone-fa5ba.web.app/'>
-                  <a className={projectStyle.thumb}>
+                  <a className={projectStyle.thumb} target='_blank'>
                     <Image
                       src={netflix}
                       layout='fill'
@@ -46,22 +83,18 @@ function Project() {
                 </div>
                 <div className={projectStyle.links}>
                   <Link href='https://netflix-clone-fa5ba.web.app/'>
-                    <a>
+                    <a target='_blank'>
                       <LaunchIcon />
-                    </a>
-                  </Link>
-                  <Link href='/'>
-                    <a>
-                      <GitHubIcon />
                     </a>
                   </Link>
                 </div>
               </div>
             </div>
+
             <div className={projectStyle.project__card}>
               <div className={projectStyle.project__thumbnail}>
                 <Link href='https://tinder-clone-4eaf2.web.app/'>
-                  <a className={projectStyle.thumb}>
+                  <a className={projectStyle.thumb} target='_blank'>
                     <Image
                       src={tinder}
                       layout='fill'
@@ -77,54 +110,16 @@ function Project() {
                   <span>A Simple Tinder MERN Stack build.</span>
                 </div>
                 <div className={projectStyle.technologies}>
-                  <span>ReactJs Nodejs </span>
+                  <span>ReactJs Nodejs MongoDB </span>
                 </div>
                 <div className={projectStyle.links}>
                   <Link href='https://tinder-clone-4eaf2.web.app/'>
-                    <a>
+                    <a target='_blank'>
                       <LaunchIcon />
                     </a>
                   </Link>
                   <Link href='https://github.com/natachigram/tinder-clone'>
-                    <a>
-                      <GitHubIcon />
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className={projectStyle.project__card}>
-              <div className={projectStyle.project__thumbnail}>
-                <Link href='/'>
-                  <a className={projectStyle.thumb}>
-                    <Image
-                      src={netflix}
-                      layout='fill'
-                      className={projectStyle.image}
-                    />
-                    <div></div>
-                  </a>
-                </Link>
-              </div>
-              <div className={projectStyle.project__description}>
-                <h4>Netflix Clone</h4>
-                <div className={projectStyle.textSummary}>
-                  <span>
-                    A Simple Netflix Frontend UI built based on reactJs and
-                    integrated with netflix movies based API.
-                  </span>
-                </div>
-                <div className={projectStyle.technologies}>
-                  <span>ReactJs Firebase </span>
-                </div>
-                <div className={projectStyle.links}>
-                  <Link href='/'>
-                    <a>
-                      <LaunchIcon />
-                    </a>
-                  </Link>
-                  <Link href='/'>
-                    <a>
+                    <a target='_blank'>
                       <GitHubIcon />
                     </a>
                   </Link>
